0021a5f54b849d80c4a851dd2af7b06eac3a65c7
FolytanAgnes <folytanagnes@gmail.com>
2025-02-03 16:22:33 +0100

    Render start

--- src/pages/webgl/Render.ts
+++ src/pages/webgl/Render.ts
@@ -1,6 +1,8 @@
 import { IndexBuffer } from "./IndexBuffer";
 import { Shader } from "./Shader";
 import { VertexArray } from "./Shaders/VertexArray ";
+import { Texture } from "./Texture";
+import { VertexBufferLayout } from "./VertxBufferLayout";
 
 interface RenderData {
     va: VertexArray;
@@ -13,14 +15,28 @@ interface RenderData {
 
 export class Render {
     private gl: WebGL2RenderingContext;
+    private layers: RenderData[] = []; 
+    private cursor: RenderData;
 
-    constructor(gl: WebGL2RenderingContext){
+    private cursorRadius: number = 0.01;
+    private initialCanvasRatio: [number, number] = [1.0, 1.0];
+    private canvasRatio: [number, number] = [1.0, 1.0];
+    private identityRatio: [number, number] = [1.0, 1.0];
+    private offset: [number, number] = [0, 0];
+    private cursorScale: [number, number, number] = [1.0, 1.0, 1.0];
+    private identityOffset: [number, number] = [0, 0];
+    private prevPos: [number, number] = [0, 0];
+    private canvasSize: [number, number] = [1, 1];
+    private fbo: WebGLFramebuffer | null = null;
+
+    constructor(gl: WebGL2RenderingContext, layout: VertexBufferLayout, indexData: Uint32Array) {
         this.gl = gl;
+        this.cursor = {
+            va: new VertexArray(this.gl, layout),        
+            ib: new IndexBuffer(this.gl, indexData, indexData.length), 
+            shader: new Shader(this.gl),                 
+            texture: new Texture(this.gl)                
+        };
     }
-    renderData: RenderData = {
-        va: new VertexArray(this.gl),
-        ib: new IndexBuffer(this.gl),
-        shader: new Shader(this.gl),
-        texture: new Texture(),
-    };
+
 }
--- src/pages/webgl/Texture.ts
+++ src/pages/webgl/Texture.ts
@@ -1,6 +1,84 @@
-export class Texture{
+export class Texture {
     private gl: WebGL2RenderingContext;
-    constructor(gl: WebGL2RenderingContext){
+    private texture: WebGLTexture;
+    private width: number = 0;
+    private height: number = 0;
+
+    constructor(gl: WebGL2RenderingContext) {
         this.gl = gl;
+        this.texture = gl.createTexture()!;
+    }
+
+    public init(path: string): void;
+    public init(width: number, height: number): void;
+
+    public init(arg1: string | number, arg2?: number): void {
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+        
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+
+        if (typeof arg1 === "string") {
+            const image = new Image();
+            image.src = arg1;
+            image.onload = () => {
+                this.width = image.width;
+                this.height = image.height;
+    
+                this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+    
+                this.gl.texImage2D(
+                    this.gl.TEXTURE_2D,
+                    0,
+                    this.gl.RGBA,
+                    this.gl.RGBA,
+                    this.gl.UNSIGNED_BYTE,
+                    image
+                );
+    
+                this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+            };
+        } else {
+            this.width = arg1;
+            this.height = arg2!;
+    
+            const data = new Uint8Array(this.width * this.height * 4);
+            for (let i = 0; i < data.length; i++) {
+                data[i] = (i + 1) % 4 === 0 ? 255 : 100;
+            }
+    
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+    
+            this.gl.texImage2D(
+                this.gl.TEXTURE_2D,
+                0,
+                this.gl.RGBA,
+                this.width,
+                this.height,
+                0,
+                this.gl.RGBA,
+                this.gl.UNSIGNED_BYTE,
+                data
+            );
+    
+            this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+        }
+
+        this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+    }
+
+    bind(slot: number): void {
+        this.gl.activeTexture(this.gl.TEXTURE0 + slot);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+    }
+    
+    unbind() {
+        this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+    }
+
+    getId(): WebGLTexture {
+        return this.texture;
     }
 }
