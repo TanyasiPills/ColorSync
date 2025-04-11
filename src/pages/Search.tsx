ec720ac64c3cb984ccc5488ff52ee25a0de56ad6
FolytanAgnes <folytanagnes@gmail.com>
2025-04-11 09:36:29 +0200

    uploading yesterday stuff

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,67 +1,87 @@
 import { useEffect, useState } from "react";
-import "../css/SocialMedia.css";
-import { Container, Row, Col, Card, Spinner } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { backendIp } from "../constants";
 import { post, user } from "../types"
 import Cookies from "universal-cookie";
+import "../css/SocialMedia.css"
+import { OnePost } from "./modals/OnePost";
 
 export function Search() {
   const [users, setUsers] = useState<user[]>([]);
   const [posts, setPosts] = useState<post[]>([]);
-  const [postsTexts, setPostsTexts] = useState<post[]>([]);
-  const [postsTags, setPostsTags] = useState<post[]>([]);
   const [searchQuery, setSearchQuery] = useState<string | undefined>(undefined);
   const [show, setShow] = useState(false);
-  const cookie = new Cookies();
+  const [postId, setPostID] = useState<number>();
+  const cookies = new Cookies();
+  const thisUser = cookies.get("AccessToken");
+  let offset: number | null = 0;
+
 
   async function loadUsers() {
     const params: any = {};
     if (searchQuery) params.name = searchQuery;
-    const result = await fetch(backendIp + '/users/search?' + new URLSearchParams(params), { method: "GET", headers: { "Accept": "application/json" } });
+    const headers: any = { "Accept": "application/json" }
+    if (thisUser) {
+      headers["Authorization"] = "Bearer " + thisUser.access_token
+    }
+    const result = await fetch(backendIp + '/users/search?' + new URLSearchParams(params), { method: "GET", headers: headers });
     if (result.ok) { setUsers(await result.json()); } else { console.log(await result.text()) }
   }
 
-  async function loadPostsText() {
-    const params: any = {};
-    if (searchQuery) params.q = searchQuery;
-    const result = await fetch(backendIp + '/posts/search?' + new URLSearchParams(params), { method: "GET", headers: { "Accept": "application/json" } });
-    if (result.ok) { let json = await result.json(); setPostsTexts(json.data); setPosts(json.data); } else { console.log(await result.text()) }
-  }
+  async function loadPosts() {
+    const tags: string[] = [];
+    let text: string = "";
+    if (searchQuery) {
+      const regex: RegExp = /#\w+/g;
+      const matches: RegExpMatchArray | null = searchQuery.match(regex);
+      if (matches) {
+        for (const match of matches) {
+          tags.push(match.toString().substring(1));
+        }
+      }
+      text = searchQuery.replace(/#\w+\s?/g, "");
+      text = text.replace(/\s+/g, "").trim();
+    }
 
-  async function loadPostsTags() {
-    const params: any = {};
-    if (searchQuery) params.tags = searchQuery;
-    const result = await fetch(backendIp + '/posts/search?' + new URLSearchParams(params), { method: "GET", headers: { "Accept": "application/json" } });
-    if (result.ok) { let json = await result.json(); setPostsTags(json.data); } else { console.log(await result.text()) }
+    const params: URLSearchParams = new URLSearchParams();
+
+    if (tags.length > 0) {
+      for (const tag of tags) {
+        params.append("tags", tag);
+      }
+    };
+    if (text) params.append("q", text);
+    if (offset) {
+      params.append("offset", offset.toString());
+    }
+
+    const result = await fetch(backendIp + '/posts/search?' + params, { method: "GET", headers: { "Accept": "application/json" } });
+    if (result.ok) {
+      const json = await result.json();
+      setPosts(prev => [...prev, ...json.data])
+      offset = json.offset;
+    } else { console.log(await result.text()) }
   }
 
   useEffect(() => {
-
+    loadPosts()
     loadUsers();
-    loadPostsText();
-    loadPostsTags();
 
-    if (postsTexts.length < postsTags.length) {
-      setPosts(postsTexts);
-    }
-    if (postsTags.length < postsTexts.length) {
-      setPosts(postsTags);
-    }
-    else {
-      setPosts(postsTexts);
+
+    const handleScroll = () => {
+      const searchDiv = document.getElementById("searchDiv");
+      if (!searchDiv) return;
+      const bottom = searchDiv.scrollHeight - searchDiv.scrollTop - searchDiv.clientHeight <= 500;
+      if (bottom) {
+        if (offset != null) loadPosts();
+      }
     }
 
-    console.log(posts);
+    const searchDiv = document.getElementById("searchDiv");
+    if (searchDiv) {
+      searchDiv.addEventListener("scroll", handleScroll);
+    }
 
-    const homeButton = document.getElementById("homeButton")!;
-    const searchButton = document.getElementById("searchButton")!;
-
-    homeButton.addEventListener("click", () => {
-      window.location.href = '/CMS';
-    });
-    searchButton.addEventListener("click", () => {
-      window.location.href = '/CMS/SRC';
-    });
   }, [searchQuery]);
 
   function Search(event: React.ChangeEvent<HTMLInputElement>) {
@@ -73,40 +93,40 @@ export function Search() {
     window.location.href = '/Profile/' + key;
   }
 
-  return (
-    <Container fluid className="vh-100 d-flex flex-column">
-      <Row className="flex-grow-1 w-100 h-100">
-        <Col xs="2" id="left" className="h-100 d-flex flex-column align-items-center py-4">
-          <h3 className="costumButtons" id="homeButton">Home</h3>
-          <h3 className="costumButtons" id="searchButton">Search</h3>
-        </Col>
-        <Col id="middle" className="h-100 d-flex justify-content-center align-items-start py-4">
-          <div id="feed">
-            <input type="text" id="search" className="form-control mb-2 text-center" placeholder="Search..." onChange={Search} />
-              <div id="userSection" className="w-100">                
-                <div id="allUsers" className="overflow-auto w-100">
-                  {users && users.map((user) => (
-                    <div className="d-flex align-items-center p-2 border-bottom justify-content-left" data-id={user.id} onClick={takeToProfile} key={user.id}>
-                      <img src={`${backendIp}/users/${user.id}/pfp`} className="rounded-circle me-2" alt="Profile" />
-                      <h5 className="mb-0">{user.username}</h5>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div id="postsSection" className="d-flex flex-column w-100 flex-grow-1 overflow-auto">
-                <Row id="drawings" className="w-100 justify-content-center">
-                  {posts && posts.map((post) => (
-                    <Col id="col" key={post.id} xs={12} sm={6} md={4} lg={4} className="d-flex justify-content-center">                    
-                      <img  src={backendIp +"/images/" + post.imageId}  className="drawing-img" alt="Post"/>                    
-                  </Col>
-                  ))}
-                </Row>
-              </div>
-            </div>          
-        </Col>
+  function handleImgPost(id: number) {
+    setShow(true);
+    setPostID(id);
+  }
 
-        <Col xs="2" id="right" className="h-100"></Col>
-      </Row>
-    </Container>
+  return (
+    <div id="searchDiv">
+      <input type="text" id="search" className="form-control mb-2 text-center" placeholder="Search..." onChange={Search} />
+      <div id="userSection">
+        <div className="allUsers">
+          {users && users.map((user) => (
+            <div data-id={user.id} onClick={takeToProfile} key={user.id}>
+              <Row className="align-items-center">
+                <Col xs="auto">
+                  <img src={`${backendIp}/users/${user.id}/pfp`} className="profile-img" alt="Profile" />
+                </Col>
+                <Col>
+                  <h5 className="username">{user.username}</h5>
+                </Col>
+              </Row>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div id="postsSection">
+        <div className="allImages">
+          {posts && posts.map((post) => (
+            <div key={post.id} className="images">
+              <img src={backendIp + "/images/" + post.imageId} alt="Post" onClick={() => { handleImgPost(post.id) }} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <OnePost show={show} onHide={() => setShow(false)} postId={postId ?? 0} />
+    </div>
   );
 }
